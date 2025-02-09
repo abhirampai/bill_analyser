61078e2b2c2c73c5d19ad954f8de7a945a5f1c34
Abhiram Pai <abhirampai1999@gmail.com>
2025-02-09 18:17:42 +0530

    Add basic image library launcher and image show module

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,27 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Button, Image, Text, View } from "react-native";
+
+import * as ImagePicker from "expo-image-picker";
 
 export default function Index() {
+  const [image, setImage] = useState<string | null>(null);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images", "videos"],
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
   return (
     <View
       style={{
@@ -9,7 +30,16 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Button title="Pick an image from camera roll" onPress={pickImage} />
+      {image && (
+        <Image
+          source={{ uri: image }}
+          style={{
+            width: 200,
+            height: 200,
+          }}
+        />
+      )}
     </View>
   );
 }
